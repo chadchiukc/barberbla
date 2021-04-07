0838a1b1f71e13d4aead503bfe496a2e649f95bd
chadchiu <chadchiu318@gmail.com>
2021-04-07 21:57:30 +0800

    test

---
+++ lib/controllers/location_controller.dart
@@ -0,0 +1,17 @@
+import 'package:barberbla/models/location_model.dart';
+import 'package:barberbla/utils/gps.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
+
+class LocationController extends GetxController {
+  var _position = LocationModel().obs;
+
+  get position => this._position.value?.lat;
+
+  void getLocation() async {
+    Position position = await GPS().determinePosition();
+    _position.value =
+        LocationModel(lat: position.latitude, lng: position.longitude);
+    ;
+  }
+}
