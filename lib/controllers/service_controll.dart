c6ce19a170fcc20e6ab0b63cbff6a64eee15e4ef
chadchiu <chadchiu318@gmail.com>
2021-04-08 23:24:33 +0800

    image issue not fixed

--- lib/controllers/service_controll.dart
+++ lib/controllers/service_controll.dart
@@ -1,5 +1,6 @@
 import 'package:barberbla/models/service_model.dart';
 import 'package:barberbla/utils/database.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -14,13 +15,17 @@ class ServiceController extends GetxController {
   }
 
   void addServices() async {
+    EasyLoading.show(status: 'test.');
     try {
       _services = await Database().getServices();
       _services.docs.forEach((element) {
+        // var ref = FirebaseStorage.instance.ref().child(element.data().);
+        print(element.data());
         servicesList.add(ServiceModel.fromQueryDocumentnapshot(element));
       });
     } catch (e) {
       Get.snackbar("Error during fetching from server", e.message);
     }
+    EasyLoading.dismiss();
   }
 }
--- lib/models/service_model.dart
+++ lib/models/service_model.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class ServiceModel {
   String id;
